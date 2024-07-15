4c195868feec1948fd33df53fe9114b90404880a
abhishekumar09 <2003abhishek.09@gmail.com>
2024-07-15 23:08:48 +0530

    resume

--- src/data/constants.js
+++ src/data/constants.js
@@ -9,7 +9,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/abhishekumar09",
   resume:
-    "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
+    "https://drive.google.com/file/d/19y6PSvFlsZ1ydDnzBLchBYX9HYq4L8DC/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/abhishekkumar009/",
   // twitter: "https://twitter.com/RishavChanda",
   insta: "https://www.instagram.com/abhishek1466yadav/",
